b1dd97d31f3d7cc2cc5d1afde38595b617e5d95f
d3byte <pandinator@protonmail.com>
2020-09-07 17:25:53 +0300

    feat: variable constructor

--- tasks/07.09.ts
+++ tasks/07.09.ts
@@ -1,7 +1,30 @@
 import {ModuleInterface} from "../interfaces/module.interface";
 
+interface StudentConstructorParams {
+    name?: string;
+    age?: number;
+    grade?: number;
+    refObj?: Student;
+}
+
 class Student {
-    constructor(private name?: string, private age?: number, private grade?: number) { }
+    private name: string;
+    private age: number;
+    private grade: number;
+
+    constructor(params: StudentConstructorParams = {}) {
+        Object
+            .entries(params)
+            .forEach(([key, value]) => {
+                if (key === 'refObj') {
+                    this.name = (value as Student).getName();
+                    this.age = (value as Student).getAge();
+                    this.grade = (value as Student).getGrade();
+                } else {
+                    this[key] = value;
+                }
+            });
+    };
 
     public getName(): string {
         return this.name;
@@ -36,8 +59,15 @@ class Student {
 
 export class StudentApp implements ModuleInterface {
     public init(): void {
-        const student1: Student = new Student("Сергей", 20, 5);
+        const student1: Student = new Student({
+            name: "Сергей",
+            age: 20,
+            grade: 5,
+        });
+
         const student2: Student = new Student();
+
+        const student3: Student = new Student({ refObj: student1 });
 
         student2.set("Йегрес", 5, 20);
 
@@ -48,5 +78,10 @@ export class StudentApp implements ModuleInterface {
 
         console.log('Student 2');
         console.log(`Age: ${student2.getAge()}\nName: ${student2.getName()}\nGrade: ${student2.getGrade()}`);
+
+        console.log('<--------------------------->');
+
+        console.log('Student 3');
+        console.log(`Age: ${student3.getAge()}\nName: ${student3.getName()}\nGrade: ${student3.getGrade()}`);
     }
 }
